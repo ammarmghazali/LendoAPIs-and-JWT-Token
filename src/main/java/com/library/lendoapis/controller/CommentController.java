c10f97fc836bc1367c7f003b59d18a7337053b45
Ammar Muhammad Ghazali <ammarmghazali@gmail.com>
2024-09-01 23:50:24 +0500

    Removed unused imports

--- src/main/java/com/library/lendoapis/controller/CommentController.java
+++ src/main/java/com/library/lendoapis/controller/CommentController.java
@@ -1,7 +1,6 @@
 package com.library.lendoapis.controller;
 
 import com.library.lendoapis.service.CommentService;
-import com.library.lendoapis.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
